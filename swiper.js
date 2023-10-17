92caf5d1f05929eee4cbd52810a6cc618b863a35
kostya <kostya1.6@yandex.ru>
2023-10-17 15:53:08 +0500

    finish

--- swiper.js
+++ swiper.js
@@ -53,20 +53,52 @@ let prodSwiper = new Swiper(".prodSwiper", {
     //     disableOnInteraction: false,
     // },
     navigation: {
-        nextEl: '.prevFotos-swiper-button-next',
-        prevEl: '.prevFotos-swiper-button-prev',
+        nextEl: '.prodSwiper-swiper-button-next',
+        prevEl: '.prodSwiper-swiper-button-prev',
     },
     breakpoints: {
         300: {
             slidesPerView: 1,
             spaceBetween: 0
         },
-        528: {
+        764: {
             slidesPerView: 2,
             spaceBetween: 0
         },
-        1560: {
+        1084: {
+            slidesPerView: 3,
+            spaceBetween: 0
+        },
+        1392: {
             slidesPerView: 4,
+            spaceBetween: 0
+        }
+    }
+});
+let archiveSwiper = new Swiper(".archiveSwiper", {
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    // },
+    navigation: {
+        nextEl: '.archiveSwiper-swiper-button-next',
+        prevEl: '.archiveSwiper-swiper-button-prev',
+    },
+    breakpoints: {
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        764: {
+            slidesPerView: 2,
+            spaceBetween: 0
+        },
+        1084: {
+            slidesPerView: 3,
+            spaceBetween: 0
+        },
+        1392: {
+            slidesPerView: 3,
             spaceBetween: 0
         }
     }
